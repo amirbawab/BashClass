c48336c9d267e79a5c01ced20412da767272a289
Amir Bawab <amirbawab@gmail.com>
2017-05-11 14:15:57 -0400

    Added built-in types

---
+++ include/bashclass/BTypes.h
@@ -0,0 +1,10 @@
+#ifndef BASHCLASS_BTYPES_H
+#define BASHCLASS_BTYPES_H
+
+// Language built-in types
+const char* TYPE_INT = "int";
+const char* TYPE_STRING = "string";
+const char* TYPE_ANY = "any";
+const char* TYPE_VOID = "void";
+
+#endif
