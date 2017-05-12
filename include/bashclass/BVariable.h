42b99b5232c52bd74275f8585914cb895227b06f
Amir Bawab <amirbawab@gmail.com>
2017-05-12 15:29:03 -0400

    Restrict var value in class dec

--- include/bashclass/BVariable.h
+++ include/bashclass/BVariable.h
@@ -7,11 +7,14 @@ class BVariable {
 private:
     std::string m_type;
     std::string m_name;
+    std::string m_value;
 public:
     std::string getName() const {return m_name;}
     void setName(std::string name) {m_name = name;}
     std::string getType() const {return m_type;}
     void setType(std::string type) {m_type = type;}
+    std::string getValue() const {return m_value;}
+    void setValue(std::string value) {m_value = value;}
 };
 
 #endif
