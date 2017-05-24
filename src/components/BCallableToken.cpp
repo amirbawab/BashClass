57df8fa3f8580fc28913ce7d409ab5a93728d376
Amir Bawab <amirbawab@gmail.com>
2017-05-24 12:08:45 -0400

    Added lexical error and semantic check for new types

--- src/components/BCallableToken.cpp
+++ src/components/BCallableToken.cpp
@@ -7,6 +7,10 @@ std::string BCallableToken::getTypeValue() const {
     } else if(m_lexicalToken->getName() == BType::DATA_TYPE_NAME_STRING
               || m_lexicalToken->getName() == BType::TYPE_NAME_BASH_SUB) {
         return BType::TYPE_VALUE_STRING;
+    } else if(m_lexicalToken->getName() == BType::DATA_TYPE_NAME_CHAR) {
+        return BType::TYPE_VALUE_CHAR;
+    } else if(m_lexicalToken->getName() == BType::DATA_TYPE_NAME_BOOLEAN) {
+        return BType::TYPE_VALUE_BOOLEAN;
     }
     return BType::UNDEFINED;
 }
