0a9de5f3686c6d61fa7ca7bc4e983071c0da8e49
Amir Bawab <amirbawab@gmail.com>
2017-05-29 18:26:51 -0400

    Added conditino to if and while

--- include/bashclass/BIf.h
+++ include/bashclass/BIf.h
@@ -5,11 +5,24 @@
 
 class BIf : public BScope {
 private:
+    std::shared_ptr<IBCallable> m_condition;
 public:
     /**
      * Get if label
      */
     std::stringstream getLabel();
+
+    /**
+     * Get condition
+     * @return condition
+     */
+    std::shared_ptr<IBCallable> getCondition() {return m_condition;}
+
+    /**
+     * Set condition
+     * @param condition
+     */
+    void setCondition(std::shared_ptr<IBCallable> condition) {m_condition = condition;}
 };
 
 #endif
--- include/bashclass/BWhile.h
+++ include/bashclass/BWhile.h
@@ -5,11 +5,24 @@
 
 class BWhile : public BScope {
 private:
+    std::shared_ptr<IBCallable> m_condition;
 public:
     /**
      * Get while label
      */
     std::stringstream getLabel();
+
+    /**
+     * Get condition
+     * @return condition
+     */
+    std::shared_ptr<IBCallable> getCondition(){return m_condition;}
+
+    /**
+     * Set condition
+     * @param condition
+     */
+    void setCondition(std::shared_ptr<IBCallable> condition) {m_condition = condition;}
 };
 
 #endif
