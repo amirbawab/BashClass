3f5424d40c08a2355b8f6a1bdb098995951d2b5b
Amir Bawab <amirbawab@gmail.com>
2017-05-31 20:55:50 -0400

    Enhanced code

--- src/components/BIf.cpp
+++ src/components/BIf.cpp
@@ -10,13 +10,14 @@ std::stringstream BIf::getLabel() {
 
 void BIf::setExpression(std::shared_ptr<IBCallable> expression) {
 
+    // Store the condition/expression
+    m_expression = expression;
+
+    // Verify the type of the expression is boolean
     std::string expressionType = expression->getTypeValueAsString();
     if(expressionType == BType::UNDEFINED) {
         std::cerr << "If statement condition cannot be of undefined type" << std::endl;
     } else if(expressionType != BType::TYPE_VALUE_BOOLEAN) {
         std::cerr << "An if condition must evaluate to a boolean instead of " << expressionType << std::endl;
     }
-
-    // Store the condition/expression anyway
-    m_expression = expression;
 }
--- src/components/BWhile.cpp
+++ src/components/BWhile.cpp
@@ -10,13 +10,14 @@ std::stringstream BWhile::getLabel() {
 
 void BWhile::setExpression(std::shared_ptr<IBCallable> expression) {
 
+    // Store the condition/expression
+    m_expression = expression;
+
+    // Verify the type of the condition is boolean
     std::string expressionType = expression->getTypeValueAsString();
     if(expressionType == BType::UNDEFINED) {
         std::cerr << "While statement condition cannot be of undefined type" << std::endl;
     } else if(expressionType != BType::TYPE_VALUE_BOOLEAN) {
         std::cerr << "A while condition must evaluate to a boolean instead of " << expressionType << std::endl;
     }
-
-    // Store the condition/expression anyway
-    m_expression = expression;
 }
