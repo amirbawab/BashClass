bb614f37f686f757d1daedf4a58b7aca817c7923
Amir Bawab <amirbawab@gmail.com>
2017-05-31 20:54:03 -0400

    Enhanced code

--- src/call/BVariableCall.cpp
+++ src/call/BVariableCall.cpp
@@ -11,26 +11,26 @@ std::string BVariableCall::getTypeValueAsString() {
 
 void BVariableCall::setExpression(std::shared_ptr<IBCallable> expression) {
 
-    if(!m_variable) {
-        std::cerr << "Expression cannot be assigned to the undefined variable "
-                  << m_lexicalToken->getValue() << std::endl;
-        return;
-    }
-
-    // Compare types
-    std::string expressionType = expression->getTypeValueAsString();
-    std::string variableType = m_variable->getType()->getValue();
-    if(!m_variable->hasKnownType()) {
-        std::cerr << "Cannot assign expression to an undefined type for variable "
-                  << m_variable->getName()->getValue() << std::endl;
-    } else if(expressionType == BType::UNDEFINED) {
-        std::cerr << "Variable " << m_variable->getName()->getValue()
-                  << " cannot be assigned an undefined expression" << std::endl;
-    } else if(variableType != BType::TYPE_VALUE_ANY && variableType != expressionType) {
-        std::cerr << "Variable " << m_variable->getName()->getValue() << " expects an expression of type "
-                  << variableType << " but given " << expressionType << std::endl;
-    }
-
     // Set expression
     m_expression = expression;
+
+    // Variable is required to compare the type of the expression
+    if(m_variable) {
+        // Compare types
+        std::string expressionType = expression->getTypeValueAsString();
+        std::string variableType = m_variable->getType()->getValue();
+        if(!m_variable->hasKnownType()) {
+            std::cerr << "Cannot assign expression to an undefined type for variable "
+                      << m_variable->getName()->getValue() << std::endl;
+        } else if(expressionType == BType::UNDEFINED) {
+            std::cerr << "Variable " << m_variable->getName()->getValue()
+                      << " cannot be assigned an undefined expression" << std::endl;
+        } else if(variableType != BType::TYPE_VALUE_ANY && variableType != expressionType) {
+            std::cerr << "Variable " << m_variable->getName()->getValue() << " expects an expression of type "
+                      << variableType << " but given " << expressionType << std::endl;
+        }
+    } else {
+        std::cerr << "Expression cannot be evaluated because it is assigned to an undefined variable "
+                  << m_lexicalToken->getValue() << std::endl;
+    }
 }
