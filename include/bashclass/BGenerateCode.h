301110a46c8621a916d7ad246534920b63f0d557
Amir Bawab <amirbawab@gmail.com>
2017-06-01 19:40:42 -0400

    Added BGenerateCode

---
+++ include/bashclass/BGenerateCode.h
@@ -0,0 +1,38 @@
+#ifndef BASHCLASS_BGENERATECODE_H
+#define BASHCLASS_BGENERATECODE_H
+
+#include <sstream>
+
+class BGenerateCode {
+private:
+
+    // Mark destructor as private
+    BGenerateCode(){}
+public:
+
+    /**
+     * Get singleton instance
+     * @return singleton instance
+     */
+    static BGenerateCode& get() {
+        static BGenerateCode instance;
+        return instance;
+    }
+
+    // Delete constructor and operator
+    BGenerateCode(BGenerateCode const&) = delete;
+    void operator=(BGenerateCode const&) = delete;
+
+    /**
+     * Code generated before parsing the input
+     */
+    void writePreCode();
+
+    /**
+     * Write code
+     * @param code
+     */
+    void write(std::stringstream code);
+};
+
+#endif
