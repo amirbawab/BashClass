c12ac2e713d9b0038089010f27cfb6733c655a7f
Amir Bawab <amirbawab@gmail.com>
2017-06-02 15:56:01 -0400

    Added block comments tto functions in BBashHelper

--- include/bashclass/BBashHelper.h
+++ include/bashclass/BBashHelper.h
@@ -9,15 +9,65 @@
 
 class BBashHelper {
 public:
+
+    /**
+     * Generate header bash code
+     */
     static void header();
+
+    /**
+     * Generate footer bash code
+     */
     static void footer();
+
+    /**
+     * Generate an associative array
+     * @param classScope
+     */
     static void declareClass(std::shared_ptr<BClass> classScope);
+
+    /**
+     * Generate a unique counter for a class
+     * @param classScope
+     */
     static void uniqueCounter(std::shared_ptr<BClass> classScope);
+
+    /**
+     * Generate a global variable in bash
+     * @param variable
+     * @param defaultValue
+     */
     static void createGlobalVar(std::shared_ptr<BVariable> variable, std::string defaultValue="");
+
+    /**
+     * Genera a local variable in bash
+     * @param variable
+     * @param defaultValue
+     */
     static void createLocalVar(std::shared_ptr<BVariable> variable, std::string defaultValue="");
+
+    /**
+     * Generate a function in bash
+     * @param function
+     */
     static void createFunction(std::shared_ptr<BFunction> function);
+
+    /**
+     * Close a function in bash
+     * @param function
+     */
     static void closeFunction(std::shared_ptr<BFunction> function);
+
+    /**
+     * Write pure bash
+     * @param lexicalToken
+     */
     static void bash(std::shared_ptr<ecc::LexicalToken> lexicalToken);
+
+    /**
+     * Generate a variable assign
+     * @param chainCall
+     */
     static void assignVariable(std::shared_ptr<BChainCall> chainCall);
 };
 
