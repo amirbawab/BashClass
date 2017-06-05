aa9590e7558add1680ba042a7dad3f5e8f625629
Amir Bawab <amirbawab@gmail.com>
2017-06-05 19:33:58 -0400

    Added BChainable

---
+++ include/bashclass/BChainable.h
@@ -0,0 +1,13 @@
+#ifndef BASHCLASS_BCHAINABLE_H
+#define BASHCLASS_BCHAINABLE_H
+
+class BChainable {
+
+    /**
+     * Get the type of the chainable element
+     * @return type value as a string
+     */
+    virtual std::string getTypeValueAsString()=0;
+};
+
+#endif
