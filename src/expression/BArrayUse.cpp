a02e14804ad5407d93930298297b4d22dfebc5dd
Amir Bawab <amirbawab@gmail.com>
2017-06-30 12:05:26 -0400

    Fixed bug in array use

--- src/expression/BArrayUse.cpp
+++ src/expression/BArrayUse.cpp
@@ -1,12 +1,16 @@
 #include <bashclass/BArrayUse.h>
-#include <bashclass/BElementType.h>
 
 BArrayUse::BArrayUse() {
     m_type = std::make_shared<BElementType>();
 }
 
 void BArrayUse::setTypeLexicalToken(std::shared_ptr<ecc::LexicalToken> lexicalToken) {
+
+    // Set the type lexical token
     m_type->setLexicalToken(lexicalToken);
+
+    // Link type
+    m_type->linkType();
 }
 
 void BArrayUse::castType(std::shared_ptr<BElementType> type) {
